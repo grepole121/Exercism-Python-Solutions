238c5b20ff001176d85c23be378fe9f8afc38085
George Repole <grepole121@gmail.com>
2021-01-04 22:57:27 +0000

    Completed prime-factors exercise

---
+++ prime-factors/prime_factors.py
@@ -0,0 +1,9 @@
+def factors(value):
+    prime_factors = []
+    divisor = 2
+    while value != 1:
+        while value % divisor == 0:
+            prime_factors.append(divisor)
+            value = int(value/divisor)
+        divisor+=1
+    return prime_factors
