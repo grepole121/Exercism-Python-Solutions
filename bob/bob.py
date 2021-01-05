8aa15f085201677a0a1e103dc91b8abd05ec1729
George Repole <grepole121@gmail.com>
2021-01-05 20:10:55 +0000

    Changed bob exercise slightly after looking at community solutions

--- bob/bob.py
+++ bob/bob.py
@@ -1,25 +1,22 @@
 import string
 
-
 responses = ["Sure.",
              "Whoa, chill out!",
              "Calm down, I know what I'm doing!",
              "Fine. Be that way!",
              "Whatever."]
 
+
 def response(hey_bob):
-    hey_bob = hey_bob.replace(" ","")
-    hey_bob = hey_bob.replace("\n","")
-    hey_bob = hey_bob.replace("\t","")
-    hey_bob = hey_bob.replace("\r","")
+    hey_bob = hey_bob.strip()
 
     if hey_bob == "":
         return responses[3]
-    elif (hey_bob[-1] == "?") and (hey_bob.isupper()):
+    elif ( hey_bob.endswith("?") ) and ( hey_bob.isupper() ):
         return responses[2]
     elif hey_bob.isupper():
         return responses[1]
-    elif hey_bob[-1] == "?":
+    elif hey_bob.endswith("?"):
         return responses[0]
-
-    return responses[4]
+    else:
+        return responses[4]
