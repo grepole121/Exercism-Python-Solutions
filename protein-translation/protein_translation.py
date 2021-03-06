b8ef3aefa0ee1bf1fb6d0ae536ced5460fe0153b
George Repole <grepole121@gmail.com>
2021-03-06 00:20:02 +0000

    Completed protein_translation exercise

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,29 @@
+TRANSLATE = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP"
+}
+
+
+def proteins(strand):
+    protein = []
+    for i in range(0, len(strand), 3):
+        codon = TRANSLATE[strand[i:i+3]]
+        if codon == "STOP":
+            return protein
+        protein.append(codon)
+    return protein
