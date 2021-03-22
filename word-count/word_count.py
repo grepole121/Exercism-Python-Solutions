2b05374020dd814846b6926757d2d3544e01656f
George Repole <grepole121@gmail.com>
2021-03-22 19:26:13 +0000

    Updated word-count exercise

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,27 +1,14 @@
-import string
+import re
+from collections import defaultdict
 
 
 def count_words(sentence):
-    word_counts = {}
     sentence = sentence.lower()
-    punctuation_without_apostrophe = string.punctuation.replace("'", "")
-    # Remove all punctuation other than apostrophes and replace with whitespace
-    for punc in punctuation_without_apostrophe:
-        sentence = sentence.replace(punc, " ")
-    # Split string into list
-    sentence = sentence.split()
 
-    for word in sentence:
-        stripped_word = word
-        # Remove all leading apostrophes
-        while stripped_word[0] == "'":
-            stripped_word = stripped_word[1:]
-        # Remove all tailing apostrophes
-        while stripped_word[-1] == "'":
-            stripped_word = stripped_word[:-1]
-        # Create key in word_counts dictionary if it doesn't exist
-        if stripped_word not in word_counts:
-            word_counts[stripped_word] = 0
-        # Increment word key by one
-        word_counts[stripped_word] += 1
+    words = re.findall(r'[a-z0-9\']+', sentence)
+
+    word_counts = defaultdict(int)
+    for word in words:
+        word_counts[(word).strip("'")] += 1
+
     return word_counts
